a5fecc1095eb77f59e82b9bae5d17886ca2d02b1
Mr-Jester-Xiii <jestersilvester.dev@gmail.com>
2026-06-29 16:09:13 +0100

    Templates built

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+// For tabs
+const triggerTabList = document.querySelectorAll('#myTab button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
